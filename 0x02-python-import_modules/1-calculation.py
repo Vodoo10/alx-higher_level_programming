cc162ff558f762f98e9680b9cef9ffd87605fd80
Vodoo10 <adnane.mouradi1234@gmail.com>
2023-08-11 14:20:11 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
@@ -10,4 +9,3 @@ if __name__ == "__main__":
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-
