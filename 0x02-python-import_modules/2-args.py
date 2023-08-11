a8516b9e0e8bcbfde5d4a27326c8edd64a482fe8
Vodoo10 <adnane.mouradi1234@gmail.com>
2023-08-11 14:56:18 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,15 +5,12 @@ if __name__ == "__main__":
 
     arg = sys.argv
     i = len(arg) - 1
-
     if i >  1:
         print("{} arguments:".format(i))
         for k in range(1, i + 1):
             print("{}: {}".format(k, arg[k]))
-
     elif i == 0:
         print("{} arguments.".format(i))
-
     else:
         print("{} argument:".format(i))
         print("{}: {}".format(i, arg[1]))
